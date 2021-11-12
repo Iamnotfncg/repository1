c4fe41818d885f54b6b14ec524a5e8fa8192328c
Iamnotfncg <gudzik.03@gmail.com>
2021-11-12 08:10:30 +0200

    Created lb4.cpp
    Description

---
+++ lb4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double number, firstDigit, secondDigit;
+    cout << "Input number: ";
+    cin >> number;
+    firstDigit = static_cast<int>(number) / 10;
+    secondDigit = static_cast<int>(number) % 10;
+    cout << "Sum: " << firstDigit + secondDigit << "\nMultiply: " << firstDigit * secondDigit << endl;
+}
